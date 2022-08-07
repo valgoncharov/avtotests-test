de20042ba8afea118c55c50980957a50f3da411d
valgoncharov <goncharovvalentyn@gmail.com>
2022-08-07 19:10:54 +0300

    FirstTest.java

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,4 +1,4 @@
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 public class FirstTest {
     @Test
@@ -8,4 +8,50 @@ public class FirstTest {
     }
 
 
-}
+
+        @BeforeAll
+        public static void startClassTest() {
+            System.out.println("FirstTest class Started!");
+        }
+
+        @BeforeEach
+       void startTest() {
+            System.out.println("Test start!");
+
+        }
+
+        @Test
+        void test1() {
+            System.out.println("test1.1");
+        }
+
+        @Test
+        void test2() {
+            System.out.println("test2.1");
+        }
+        @Test
+        void test3() {
+        System.out.println("test3.1");
+        }
+        @Test
+        void test4() {
+        System.out.println("test4.1");
+        }
+        @Test
+        void test5() {
+        System.out.println("test5.1");
+        }
+
+        @AfterEach
+        void finishTast() {
+            System.out.println("Test finished!");
+        }
+
+        @AfterAll
+        public static void finishedTestAll() {
+            System.out.println("All test in FirstTest finished!");
+        }
+
+    }
+
+
